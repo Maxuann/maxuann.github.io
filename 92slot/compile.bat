a227633050149022ebadba2335a97812f38c6f5f
Maxuann <62932596+Maxuann@users.noreply.github.com>
2024-05-22 23:15:19 +0800

    Delete 92slot directory

--- 92slot/compile.bat
+++
@@ -1,4 +0,0 @@
-start /b lessc --clean-css css\main.less css\main.css
-start /b lessc --clean-css css\guide.less css\guide.css
-
-start /b uglifyjs js\load.js js\time.js js\utils.js js\main.js -m -o js\main.min.js
--- 92slot/css/css-min.bat
+++
@@ -1,3 +0,0 @@
-start /b lessc --clean-css pc.less pc.css
-start /b lessc --clean-css phone.less phone.css
-start /b lessc --clean-css guide.less guide.css
